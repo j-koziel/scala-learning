e3e08d5c7ac3ee0f6b0ae1c49854d99b62011ec5
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-01-08 20:42:42 +0000

    Variables and data types

---
+++ 03 - variables and data types/varsAndData.scala
@@ -0,0 +1,17 @@
+// There are two types of variables
+// Mutable and immutable
+// Data types can be inferred
+
+// Immutable and explicit type
+val x: Int = 10
+
+// Mutable and implicit type
+var y = 15
+// y = 20
+
+
+// String interpolation
+val firstName = "John"
+val lastName = "Doe"
+
+def output() = println(s"$firstName $lastName")
