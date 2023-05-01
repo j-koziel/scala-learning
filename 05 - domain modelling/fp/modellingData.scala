3a7cd9ba2373820154680dc315ef1a3f196d3dc7
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-05-01 20:42:07 +0100

    modelling operations for fp

--- 05 - domain modelling/fp/modellingData.scala
+++ 05 - domain modelling/fp/modellingData.scala
@@ -20,6 +20,29 @@ case class Pizza(
     toppings: Seq[Topping]
                 )
 
+// Modelling the operations
+// Operations on data are seperated from the data itself
+// Simply, you just write methods which do operations on the data.
+def pizzaPrice(p: Pizza): Double = p match
+    case Pizza(crustSize, crustType, toppings) ⇒
+        val base = 6.00
+        val crust = crustPrice(crustSize, crustType)
+        val tops = toppings.map(toppingPrice).sum
+        base + crust + tops
+
+def toppingPrice(t: Topping): Double = t match
+    case csae Cheese | Onions ⇒ 0.5
+    case Pepporoni | BlackOlives | GreenOlives ⇒ 0.75
+
+
+def crustPrice(s: CrustSize, t: CrustType): Double =
+    (s, t) match
+        case (Small | Medium, _) ⇒ 0.25
+        case (Large, Thin) ⇒ 0.50
+        case (Large, Regular) ⇒ 0.75
+        case (Large, Thick) ⇒ 1.00
+
+
 val myFavPizza = Pizza(Small, Regular, Seq(Cheese, Pepperoni))
 
 @main def output(): Unit = {
